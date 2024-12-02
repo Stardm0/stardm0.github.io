3687a2636f519accaa0de7e877a8606ae2d87ebd
Stardm0 <2581793700@qq.com>
2024-12-02 14:27:03 +0800

    Site updated: 2024-12-02 14:27:02

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733120763343";
+const VERSION = "1733120822234";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
