57c07023f7d651cfd68213b8407b1b62ff388900
Stardm0 <2581793700@qq.com>
2024-12-25 22:30:49 +0800

    Site updated: 2024-12-25 22:30:49

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1735136864635";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1735137049138";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
